752dae2de2c42d42b108dd95e8d95e2e3e805dc8
Shikhar Bansal <shikharbansal007@gmail.com>
2024-03-10 10:26:38 +0530

    Introduce Backend Route nextJs

---
+++ app/api/user/route.ts
@@ -0,0 +1,14 @@
+export function GET() {
+  return Response.json({
+    email: "shikharbansal@gmail.com",
+    name: "Shikhar Bansal",
+  });
+}
+
+export function POST() {
+    return Response.json({
+      email: "shikharbansal@gmail.com",
+      name: "Shikhar Bansal",
+    });
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,23 @@ import Image from "next/image";
 
 async function getUserDetails() {
   const response = await axios.get(
-    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+    "http://localhost:3000/api/user"
   );
   return response.data;
 }
 
 export default async function Home() {
   const userData = await getUserDetails();
+
   return (
-    <div>
-      {userData.email}
-      {userData.name}
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData?.name}</div>
+
+          {userData?.email}
+        </div>
+      </div>
     </div>
   );
 }
