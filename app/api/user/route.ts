277100a98606185faeb641e193a2836a6d890985
Shikhar Bansal <shikharbansal007@gmail.com>
2024-03-15 13:36:51 +0530

    Implemented SignUp POST req in Nextjs

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -8,8 +8,15 @@ export function GET() {
 }
 
 export async function POST(req: NextRequest) {
+  //body, express.json()
   const body = await req.json();
-console.log(body)
+  console.log(body);
+  //header
+  console.log(req.headers.get("authorization"));
+  //query parameters
+  console.log(req.nextUrl.searchParams.get("name"));
+
+  //hit the database with username, password
   return Response.json({
     message: "You logged in",
   });
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,6 @@ export default async function Home() {
       <div className="flex justify-center">
         <div className="border p-8 rounded">
           <div>Name: {userData?.name}</div>
-
           {userData?.email}
         </div>
       </div>
